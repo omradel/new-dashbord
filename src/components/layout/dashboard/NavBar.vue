7359818270e45a0437fb56547db5dca5eaf49452
omr <omradelbakry375@gmail.com>
2025-08-05 00:59:20 +0300

    feat(themeSwitcher): add theme switcher

--- src/components/layout/dashboard/NavBar.vue
+++ src/components/layout/dashboard/NavBar.vue
@@ -2,20 +2,24 @@
   <header
     class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"
   >
-    <div class="flex items-center gap-2 px-4">
+    <div class="flex items-center gap-2 px-4 w-full">
       <SidebarTrigger class="-ml-1" />
       <Separator orientation="vertical" class="mr-2 h-4" />
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem class="hidden md:block">
-            <BreadcrumbLink href="#"> {{ $route.name }} </BreadcrumbLink>
-          </BreadcrumbItem>
-          <!-- <BreadcrumbSeparator class="hidden md:block" /> -->
-          <!-- <BreadcrumbItem v-if="$route?.meta">
+      <div class="flex items-center justify-between w-full">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem class="hidden md:block">
+              <BreadcrumbLink href="#"> {{ $route.name }} </BreadcrumbLink>
+            </BreadcrumbItem>
+            <!-- <BreadcrumbSeparator class="hidden md:block" /> -->
+            <!-- <BreadcrumbItem v-if="$route?.meta">
             <BreadcrumbPage>Data Fetching</BreadcrumbPage>
           </BreadcrumbItem> -->
-        </BreadcrumbList>
-      </Breadcrumb>
+          </BreadcrumbList>
+        </Breadcrumb>
+
+        <ThemeSwitcher class="ml-2" />
+      </div>
     </div>
   </header>
 </template>
@@ -23,6 +27,7 @@
 <script setup lang="ts">
 import { SidebarTrigger } from '@/components/ui/sidebar'
 import { Separator } from 'reka-ui'
+import ThemeSwitcher from '../ThemeSwitcher.vue'
 import {
   Breadcrumb,
   BreadcrumbItem,
