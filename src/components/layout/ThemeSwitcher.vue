f16fbfa8a5fd3b0864b76fe2b00e4a17dcb46514
omr <omradelbakry375@gmail.com>
2025-08-09 13:17:24 +0300

    chor: refactor theme switcher and lang switcher

--- src/components/layout/ThemeSwitcher.vue
+++ src/components/layout/ThemeSwitcher.vue
@@ -1,39 +1,26 @@
 <template>
-  <DropdownMenu>
-    <DropdownMenuTrigger as-child>
-      <Button variant="outline" size="icon" class="border-0 dark:bg-transparent shadow-none">
-        <Moon
-          width="0rem"
-          class="h-[0rem] w-[0rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-        />
+  <Button
+    variant="outline"
+    size="icon"
+    class="border-0 dark:bg-transparent shadow-none"
+    @click="mode = mode === 'dark' ? 'light' : 'dark'"
+  >
+    <Moon
+      width="0rem"
+      class="h-[0rem] w-[0rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+    />
 
-        <Sun
-          class="absolute h-[1rem] w-[1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-        />
-
-        <span class="sr-only">Toggle theme</span>
-      </Button>
-    </DropdownMenuTrigger>
-    <DropdownMenuContent align="end">
-      <DropdownMenuItem @click="mode = 'light'"> Light </DropdownMenuItem>
-      <DropdownMenuItem @click="mode = 'dark'"> Dark </DropdownMenuItem>
-      <DropdownMenuItem @click="mode = 'auto'"> System </DropdownMenuItem>
-    </DropdownMenuContent>
-  </DropdownMenu>
+    <Sun
+      class="absolute h-[1rem] w-[1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+    />
+  </Button>
 </template>
 
 <script setup lang="ts">
 import { useColorMode } from '@vueuse/core'
 import { Button } from '@/components/ui/button'
 import { Moon, Sun } from 'lucide-vue-next'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-
 const mode = useColorMode({
-  initialValue: 'auto',
+  initialValue: 'light',
 })
 </script>
